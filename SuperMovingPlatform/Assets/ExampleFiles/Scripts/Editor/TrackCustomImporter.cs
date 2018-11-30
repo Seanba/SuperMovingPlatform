da0527b9d5b0c4396569b16f3ba2b7251846fd32
SeanB-22ct <seanb@22ndtoys.com>
2018-11-30 10:27:33 -0600

    Working example of moving platform

---
+++ SuperMovingPlatform/Assets/ExampleFiles/Scripts/Editor/TrackCustomImporter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+using SuperTiled2Unity;
+using SuperTiled2Unity.Editor;
+
+namespace SuperMovingPlatform
+{
+    public class TrackCustomImporter : CustomTmxImporter
+    {
+        private TmxAssetImportedArgs m_ImportedArgs;
+
+        public override void TmxAssetImported(TmxAssetImportedArgs args)
+        {
+            m_ImportedArgs = args;
+            InstantiateMovingPlatforms();
+        }
+
+        private GameObject InstantiateResource(string name, SuperObject marker)
+        {
+            var go = Resources.Load("MovingPlatform");
+
+            if (go != null)
+            {
+                return UnityEngine.Object.Instantiate(go, marker.transform) as GameObject;
+            }
+            else
+            {
+                m_ImportedArgs.AssetImporter.ReportError("MovingPlatform resource not found.");
+            }
+
+            return null;
+        }
+
+        private void InstantiateMovingPlatforms()
+        {
+            // Find all Tiled Objects in our map that are the moving platform type
+            var platforms = m_ImportedArgs.ImportedSuperMap.GetComponentsInChildren<SuperObject>().Where(o => o.m_Type == "MovingPlatform");
+
+            // Find all tracks in our maps. These are edge colliders under the "Track" layer
+            var tracks = m_ImportedArgs.ImportedSuperMap.GetComponentsInChildren<EdgeCollider2D>().Where(c => c.gameObject.layer == LayerMask.NameToLayer("Track"));
+
+            foreach (var marker in platforms)
+            {
+                // Instantiate the sprite for our moving platform
+                var goPlatfrom = InstantiateResource("MovingPlatform", marker);
+                if (goPlatfrom == null)
+                    continue;
+
+                var compPlatform = goPlatfrom.GetComponent<MovingPlatformOnTrack>();
+
+                // Initialize the data // fixit - properties and stuff
+                //var props = marker.GetComponent<SuperCustomProperties>();
+                //props.try
+
+                foreach (var track in tracks)
+                {
+                    compPlatform.AssignTrackIfClose(track);
+                }
+            }
+        }
+    }
+}
---
+++ SuperMovingPlatform/Assets/ExampleFiles/Scripts/MovingPlatformOnTrack.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace SuperMovingPlatform
+{
+    public class MovingPlatformOnTrack : MonoBehaviour
+    {
+        // How close we need to be to a track to start off attached to it
+        private const float MaxDistanceFromTrack = 16.0f;
+
+        public bool m_IsCounterClockwise = true; // fixit - hook up and override with properties
+        public float m_VelocityPixelsPerSecond = 64.0f; // fixit - control through properties
+
+        // This platform is always placed between two points on a track
+        // fixit - make private but serializable
+        public Vector2[] m_Points;
+        public int m_CurrentPointIndex = -1;
+
+        public bool AssignTrackIfClose(EdgeCollider2D track)
+        {
+            if (m_CurrentPointIndex != -1)
+            {
+                // Already assigned to a track
+                return false;
+            }
+
+            var points = track.points;
+            Assert.IsTrue(points.Length > 1);
+
+            var pos = gameObject.transform.position;
+            var minDistance = float.MaxValue;
+            var ptOnTrack = Vector2.zero;
+            var ptIndex = -1;
+
+            // Find closest position in the line segments passed in
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                var A = points[i];
+                var B = points[i + 1];
+                var ptPotential = ClosestPointOnLineSegment(pos, A, B);
+
+                var distance = Vector2.Distance(pos, ptPotential);
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    ptOnTrack = ptPotential;
+                    ptIndex = i;
+                }
+            }
+
+            // Are we close enough to the track to be attached to it?
+            if (minDistance < MaxDistanceFromTrack)
+            {
+                m_CurrentPointIndex = ptIndex;
+                m_Points = points;
+                gameObject.transform.position = ptOnTrack;
+                return true;
+            }
+
+            return false;
+        }
+
+        private Vector2 ClosestPointOnLineSegment(Vector2 P, Vector2 A, Vector2 B)
+        {
+            var P2 = new Vector2(B.x - A.x, B.y - A.y);
+            var dot = P2.x * P2.x + P2.y * P2.y;
+            var u = ((P.x - A.x) * P2.x + (P.y - A.y) * P2.y) / dot;
+
+            if (u > 1)
+            {
+                u = 1;
+            }
+            else if (u < 0)
+            {
+                u = 0;
+            }
+
+            return A + (u * P2);
+        }
+
+        private void Update()
+        {
+            if (m_CurrentPointIndex == -1)
+            {
+                Debug.LogError("Platform is not attached to a track.");
+                return;
+            }
+
+            float t = 1.0f;
+            while (t > 0.0f)
+            {
+                t = MoveAlongTrack(t);
+            }
+        }
+
+        private float MoveAlongTrack(float t)
+        {
+            // fixit - use direction from properties
+
+            // Move along an edge of our track as much as we can
+            // If we end up stopping at an edge then return the portion of movement that is left over
+            int i = m_CurrentPointIndex;
+            int j = (m_CurrentPointIndex + 1) % m_Points.Length;
+
+            var A = m_Points[i];
+            var B = m_Points[j];
+
+            var BA = B - A;
+            var dv = BA.normalized;
+
+            var posCurrent = (Vector2)gameObject.transform.position;
+            var posDesired = posCurrent + (dv * m_VelocityPixelsPerSecond * Time.deltaTime * t);
+
+            var V1A = posCurrent - A;
+            var V2A = posDesired - A;
+
+            float dotLimit = Vector2.Dot(dv, BA);
+            float dotStart = Vector2.Dot(dv, V1A);
+            float dotDesired = Vector2.Dot(dv, V2A);
+
+            if (dotDesired < dotLimit)
+            {
+                // We are within the bounds of the edge we are moving across
+                // Fully move to our desired position
+                gameObject.transform.position = posDesired;
+                return 0;
+            }
+            else
+            {
+                // Our desired position is out out bounds
+                // Lock to end position
+                gameObject.transform.position = B;
+
+                // Advance to the next edge in our track
+                m_CurrentPointIndex = j;
+
+                // How much movement do we have left over as a ratio?
+                float leftOverRatio = (dotDesired - dotLimit) / (dotDesired - dotStart);
+                return leftOverRatio * t;
+            }
+        }
+    }
+}
